521036c9108b07ce2726cc10bed99eeb1b0858e5
rfaze07 <renzofazele@gmail.com>
2023-07-04 15:43:53 -0300

    Se creo el menu lateral con el logo de la empresa

--- Seguimiento/Form1.Designer.vb
+++ Seguimiento/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class FrmPrincipal
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,16 +22,65 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmPrincipal))
+        Me.PanelMenu = New System.Windows.Forms.Panel()
+        Me.PanelLogo = New System.Windows.Forms.Panel()
+        Me.pbLogo = New System.Windows.Forms.PictureBox()
+        Me.PanelMenu.SuspendLayout()
+        Me.PanelLogo.SuspendLayout()
+        CType(Me.pbLogo, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Form1
+        'PanelMenu
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.PanelMenu.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(163, Byte), Integer), CType(CType(82, Byte), Integer))
+        Me.PanelMenu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PanelMenu.Controls.Add(Me.PanelLogo)
+        Me.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PanelMenu.ForeColor = System.Drawing.SystemColors.Control
+        Me.PanelMenu.Location = New System.Drawing.Point(0, 0)
+        Me.PanelMenu.Name = "PanelMenu"
+        Me.PanelMenu.Size = New System.Drawing.Size(230, 719)
+        Me.PanelMenu.TabIndex = 0
+        '
+        'PanelLogo
+        '
+        Me.PanelLogo.Controls.Add(Me.pbLogo)
+        Me.PanelLogo.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelLogo.Location = New System.Drawing.Point(0, 0)
+        Me.PanelLogo.Name = "PanelLogo"
+        Me.PanelLogo.Size = New System.Drawing.Size(228, 149)
+        Me.PanelLogo.TabIndex = 0
+        '
+        'pbLogo
+        '
+        Me.pbLogo.BackColor = System.Drawing.SystemColors.Control
+        Me.pbLogo.BackgroundImage = CType(resources.GetObject("pbLogo.BackgroundImage"), System.Drawing.Image)
+        Me.pbLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.pbLogo.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pbLogo.Location = New System.Drawing.Point(0, 0)
+        Me.pbLogo.Name = "pbLogo"
+        Me.pbLogo.Size = New System.Drawing.Size(228, 149)
+        Me.pbLogo.TabIndex = 0
+        Me.pbLogo.TabStop = False
+        '
+        'FrmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 19.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(849, 492)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(1132, 719)
+        Me.Controls.Add(Me.PanelMenu)
+        Me.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "FrmPrincipal"
+        Me.PanelMenu.ResumeLayout(False)
+        Me.PanelLogo.ResumeLayout(False)
+        CType(Me.pbLogo, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents PanelMenu As Panel
+    Friend WithEvents PanelLogo As Panel
+    Friend WithEvents pbLogo As PictureBox
 End Class
--- Seguimiento/Form1.vb
+++ Seguimiento/Form1.vb
@@ -1,3 +1,5 @@
-﻿Public Class Form1
-
+﻿Public Class FrmPrincipal
+    Private Sub FrmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
 End Class
